48da913c8862df032e88e3bfadf2d325d9a69086
jay75chauhan <chauhan75jayk@gmail.com>
2021-05-05 15:23:54 +0530

    initial commit

---
+++ src/Nav.js
@@ -0,0 +1,38 @@
+import "./Nav.css";
+
+import React, { useEffect, useState } from "react";
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <div className="nav_contents">
+        <img
+          className="nav_logo"
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          //"https://assets.brand.microsites.netflix.io/assets/493f5bba-81a4-11e9-bf79-066b49664af6_cm_1440w.png?v=26"
+          alt=""
+        />
+        <img
+          className="nav_avatar"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDU0XKaw5GSUZC9blvYPH5dXI5hSPvXbKE",
+  authDomain: "netflix-clone-redux-f59be.firebaseapp.com",
+  projectId: "netflix-clone-redux-f59be",
+  storageBucket: "netflix-clone-redux-f59be.appspot.com",
+  messagingSenderId: "9831328850",
+  appId: "1:9831328850:web:59607a9d190fea6553838b",
+};
