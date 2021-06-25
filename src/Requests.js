18611d52c1e66d406811fa64880c956fcbce0b5c
jay75chauhan <chauhan75jayk@gmail.com>
2021-06-25 20:12:20 +0530

    env problem

--- src/Requests.js
+++ src/Requests.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = "e2914f74f6bbde3926e25851c5fa6f36";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
--- src/screens/Fullpage.js
+++ src/screens/Fullpage.js
@@ -9,7 +9,7 @@ import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
 
 function Fullpage({ match }) {
-  const API_KEY = process.env.REACT_APP_API_KEY;
+  // const API_KEY = process.env.API_KEY;
   const { id } = useParams();
   const [movie, setMovie] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
@@ -18,7 +18,7 @@ function Fullpage({ match }) {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(
-        `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
+        `https://api.themoviedb.org/3/movie/${id}?api_key=e2914f74f6bbde3926e25851c5fa6f36`
       );
       const { data } = request;
       setMovie(data);
