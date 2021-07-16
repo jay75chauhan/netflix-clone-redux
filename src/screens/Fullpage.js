0329118697e5c881534e1335fd562187e788b1ba
jay75chauhan <chauhan75jayk@gmail.com>
2021-07-16 16:59:57 +0530

    string add

--- src/screens/Fullpage.js
+++ src/screens/Fullpage.js
@@ -9,7 +9,7 @@ import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
 
 function Fullpage({ match }) {
-  const API_KEY = e2914f74f6bbde3926e25851c5fa6f36;
+  const API_KEY = "e2914f74f6bbde3926e25851c5fa6f36";
   // const API_KEY = process.env.API_KEY;
 
   const { id } = useParams();
